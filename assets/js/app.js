73a4399948c24569c95b57d01fbe057ca9d42cb6
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-07-19 13:42:21 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,37 @@
 		}
 	}
 
+	let sliderHotDeals = () => {
+		new Swiper('#slider-hotDeals .swiper', {
+			spaceBetween: 15,
+			speed: 1000,
+			loop: !1,
+			navigation: {
+				nextEl: '#slider-hotDeals .button-next',
+				prevEl: '#slider-hotDeals .button-prev',
+			},
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+				},
+				375: {
+					slidesPerView: 1.5,
+				},
+				600: {
+					slidesPerView: 2.5,
+				},
+				991: {
+					slidesPerView: 3.5,
+				},
+				1499: {
+					slidesPerView: 4,
+				}
+			}
+		});
+	}
+
 	$(function () {
+		sliderHotDeals();
 	});
 
 })(jQuery);
